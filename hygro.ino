d508866703a3b8a07ecc411799ab8ff44d89edb7
liquidrinu <liquidrinu@users.noreply.github.com>
2018-03-11 00:29:09 +0100

    Update hygro.ino

--- hygro.ino
+++ hygro.ino
@@ -1,45 +1,47 @@
 //constants
 const int hygrometer = A7;
 
-// leds
-const int sa1 = A0;
-const int sa2 = A1;
-const int sb1 = A2;
-const int sb2 = A3;
+int value = 0;
+int treshold = 20;
 
-int value;
+// leds
+const int sa1 = A0; // treshold
+const int sa2 = A1; // value
 
 #include "DHT.h"
-#define DHTPIN 6     // what pin we're connected to
-#define DHTTYPE DHT11   // DHT 11 
+#define DHTPIN 6 // pinDATA
+#define DHTTYPE DHT11 // sensor
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
+
   Serial.begin(9600);
   dht.begin();
 
   pinMode(sa1, OUTPUT);
   pinMode(sa2, OUTPUT);
-  pinMode(sb1, OUTPUT);
-  pinMode(sb2, OUTPUT);
 }
 
 void loop() {
 
-  //temperature
+  // Temperature + Humidity
   float h = dht.readHumidity();
   // Read temperature as Celsius
   float t = dht.readTemperature();
 
-  // Check if any reads failed and exit early (to try again).
+  // Test
   if (isnan(h) || isnan(t) ) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
 
-  // prints
-  Serial.write(12);       // clear terminal
+  // Hygrometer
+  value = analogRead(hygrometer);
+  value = constrain(value, 400, 1023);
+  value = map(value, 400, 1023, 100, 0);
 
+  // Prints
+  Serial.write(12); // clear terminal
   Serial.println("-----------------------");
   Serial.print(" Air Humidity: ");
   Serial.print(h);
@@ -47,24 +49,33 @@ void loop() {
   Serial.print(" Temperature: ");
   Serial.print(t);
   Serial.println(" *C ");
-
-  // Hygrometer
-  value = analogRead(hygrometer);
-  value = constrain(value, 400, 1023); //Keep the ranges!
-  value = map(value, 400, 1023, 100, 0);
   Serial.print(" Soil: ");
   Serial.print(value);
-  Serial.println("%");
+  Serial.print("%");
+  if (value < treshold) {
+    Serial.println(" ** Needs watering! **");
+  } else {
+    Serial.println("");
+  }
   Serial.println("-----------------------");
 
   // led monitor for soil
-  if (value < 20) {
-    digitalWrite(sa1, HIGH);
+  if (value < treshold) {
     digitalWrite(sa2, LOW);
+    for (int i = 0; i < 10; i++) {
+      // amount of blinks
+      for (int j = 0; j < 2; j++) {
+        digitalWrite(sa1, HIGH);
+        delay(100);
+        digitalWrite(sa1, LOW);
+        delay(100);
+      }
+      delay(600);
+    }
   } else {
     digitalWrite(sa1, LOW);
     digitalWrite(sa2, HIGH);
+    delay(10000);
   }
-  delay(10000); //2 sec for humidity/temp
-}
 
+}
