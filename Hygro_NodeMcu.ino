94126d8c11207e7933a8472c11d6310930eb5326
liquidrinu <liquidrinu@users.noreply.github.com>
2018-03-19 18:30:33 +0100

    Update Hygro_NodeMcu.ino

--- Hygro_NodeMcu.ino
+++ Hygro_NodeMcu.ino
@@ -3,8 +3,8 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-const char* ssid = "Woudvrucht";
-const char* password = "Y0uSh@llN0tP@ss";
+const char* ssid = "secret;
+const char* password = "secret";
 
 ESP8266WebServer server(80);
 const int led = 4;
