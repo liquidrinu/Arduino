dde3ec9fd3276f4c7366ac280e8445848fcd479d
liquidrinu <liquidrinu@users.noreply.github.com>
2018-06-20 00:39:01 +0200

    Update D8_PORTAL.ino

--- code/D8_PORTAL.ino
+++ code/D8_PORTAL.ino
@@ -9,12 +9,11 @@ ESP8266WebServer server(80);
 
 // Captive portal
 #include <AutoConnect.h>
+AutoConnect Portal(server);
+AutoConnect portal;
 
 // HTML
 #import "index.h"
-
-AutoConnect Portal(server);
-AutoConnect        portal;
 
 // general settings
 int power = true;
@@ -23,8 +22,8 @@ int power = true;
 unsigned long previousMillis_soil = 0;
 const long interval_soil = 300000; // 5 minutes default
 
-const int inBetweenies = 100;   // delays between each read
-const int numReadings = 15;     // Increase to smooth more, but will slow down readings
+const int inBetweenies = 100; // delays between each read
+const int numReadings = 15;   // Increase to smooth more, but will slow down readings
 
 //    The interval between each read is 100ms, so 15 x 100 = 1500ms.
 //    This needs to be lower than "interval_soil" + 1000ms, or
@@ -47,39 +46,39 @@ const int hygroJuice = 15; // pin power
 // leds
 const int brightness = 55; // 0 = off, 255 = fully lit
 
-const int sa1 = 2; // [treshold]
+const int sa1 = 2;  // [treshold]
 const int sa2 = 13; // [value]
-const int sa3 = 0; // [standbyali]
+const int sa3 = 0;  // [standbyali]
 
 // LCD
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x3f, 20, 4); // (memory address, columns, rows);
 
-// capacative touch sensor
+// Capacative touch sensor
 int TouchSensor = 14;
 boolean currentState = LOW;
 boolean lastState = LOW;
-boolean LedState = LOW;
+boolean lightState = LOW;
 
 // Smoothing variables
-int readings[numReadings];      // the readings from the analog input
-int readIndex = 0;              // the index of the current reading
-int total = 0;                  // the running total
-int average = 0;                // the average
-int soil_avg;                   // the smoothed output for all the functions
-
+int readings[numReadings]; // the readings from the analog input
+int readIndex = 0;         // the index of the current reading
+int total = 0;             // the running total
+int average = 0;           // the average
+int soil_avg;              // the smoothed output for all the functions
 
 // humidity/temp
 #include <Adafruit_Sensor.h>
 #include "DHT.h"
-#define DHTPIN 12 // pinDATA
+#define DHTPIN 12     // pinDATA
 #define DHTTYPE DHT11 // sensor
 DHT dht(DHTPIN, DHTTYPE);
 
 // end config  //
 ///////////////////////////////////////////////////////////////////////////////
 
-void handleRoot() {
+void handleRoot()
+{
 
   String homePage = MAIN_page;
   server.send(200, "text/html", homePage);
@@ -88,7 +87,8 @@ void handleRoot() {
   digitalWrite(sa3, 0);
 }
 
-void handleNotFound() {
+void handleNotFound()
+{
   digitalWrite(sa3, 1);
   String message = "File Not Found\n\n";
   message += "URI: ";
@@ -98,14 +98,16 @@ void handleNotFound() {
   message += "\nArguments: ";
   message += server.args();
   message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) {
+  for (uint8_t i = 0; i < server.args(); i++)
+  {
     message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
   }
   server.send(404, "text/plain", message);
   digitalWrite(sa3, 0);
 }
 
-void setup(void) {
+void setup(void)
+{
 
   // lcd
   lcd.begin();
@@ -135,17 +137,21 @@ void setup(void) {
 
   server.on("/lights", lights);
 
+  server.on("/data", ajax);
+
   server.onNotFound(handleNotFound);
 
   Portal.begin();
 
   // smoothing init
-  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+  for (int thisReading = 0; thisReading < numReadings; thisReading++)
+  {
     readings[thisReading] = 0;
   }
 
   // error handling for soil timings
-  if ((numReadings * inBetweenies) + 1000 > interval_soil) {
+  if ((numReadings * inBetweenies) + 1000 > interval_soil)
+  {
     Serial.println("");
     Serial.println("ERROR: Your timing on 'interval' is ");
     Serial.print("too low for the amount of 'numReadings'");
@@ -159,20 +165,27 @@ void setup(void) {
   soil_readings();
 }
 
-void loop(void) {
+///////////////////////
+// M A I N   L O O P //
+///////////////////////
+
+void loop(void)
+{
 
   Portal.handleClient();
 
   unsigned long currentMillis_soil = millis();
   unsigned long currentMillis_dht = millis();
 
-  if (currentMillis_soil - previousMillis_soil >= interval_soil) {
+  if (currentMillis_soil - previousMillis_soil >= interval_soil)
+  {
     previousMillis_soil = currentMillis_soil;
     soil_readings();
     serial_print();
   }
 
-  if (currentMillis_dht - previousMillis_dht >= interval_dht) {
+  if (currentMillis_dht - previousMillis_dht >= interval_dht)
+  {
     previousMillis_dht = currentMillis_dht;
     dht_readings();
   }
@@ -182,7 +195,6 @@ void loop(void) {
   lcd_out();
   wifi_out();
   stdby();
-
 }
 
 ////////////////////////
@@ -202,12 +214,14 @@ int temp;
 int reading_passes; // soil iteratiosn while  loop
 
 // soil readings
-int soil_readings() {
+int soil_readings()
+{
 
   // Hygrometer
   digitalWrite(hygroJuice, HIGH);
 
-  if (power == true) {
+  if (power == true)
+  {
     analogWrite(sa3, brightness);
   }
 
@@ -215,11 +229,13 @@ int soil_readings() {
   Serial.println("Amount of numreadings = ");
   Serial.println(numReadings);
 
-  if (digitalRead(hygroJuice) == HIGH ) {
+  if (digitalRead(hygroJuice) == HIGH)
+  {
 
     reading_passes = 0;
 
-    while (reading_passes <  numReadings ) {
+    while (reading_passes < numReadings)
+    {
 
       delay(inBetweenies);
 
@@ -233,7 +249,6 @@ int soil_readings() {
 
       soil_phase_print();
       soil_error();
-
     }
 
     Serial.println("loop ended");
@@ -244,10 +259,10 @@ int soil_readings() {
     digitalWrite(hygroJuice, LOW);
     analogWrite(sa3, 0);
   }
-
 }
 
-int dht_readings() {
+int dht_readings()
+{
 
   // Humidity + Temperature
   float a = dht.readHumidity();
@@ -257,25 +272,26 @@ int dht_readings() {
   previousHumidity = currentHumidity;
   previousTemperature = currentTemperature;
 
-  if (!isnan(a) || !isnan(b)) {
+  if (!isnan(a) || !isnan(b))
+  {
 
     currentHumidity = a;
     currentTemperature = b;
 
     humid = currentHumidity;
     temp = currentTemperature;
-
-  } else {
+  }
+  else
+  {
 
     humid = previousHumidity;
     temp = previousTemperature;
-
   }
-
 }
 
 // Serial OUT
-int serial_print() {
+int serial_print()
+{
 
   Serial.write(12); // clear terminal
   Serial.println("-----------------------");
@@ -289,18 +305,21 @@ int serial_print() {
   Serial.print(soil_avg);
   Serial.print("%");
 
-  if (value < treshold) {
+  if (value < treshold)
+  {
     Serial.println(" ** Needs watering! **");
-  } else {
+  }
+  else
+  {
     Serial.println("");
   }
 
   Serial.println("-----------------------");
-
 }
 
 //WIFI OUT
-int wifi_out() {
+int wifi_out()
+{
 
   String mesg = "";
   mesg += " Air Humidity : '";
@@ -317,15 +336,17 @@ int wifi_out() {
   mesg += ";\n";
 
   server.send(200, "text/plain", mesg);
-
 }
 
 // synced leds
-int sync_leds() {
+int sync_leds()
+{
 
-  if (power == true) {
+  if (power == true)
+  {
 
-    if (soil_avg < treshold) {
+    if (soil_avg < treshold)
+    {
       analogWrite(sa2, 0);
       analogWrite(sa3, 0);
 
@@ -333,38 +354,46 @@ int sync_leds() {
       delay(100);
       analogWrite(sa1, 0);
       delay(100);
-    } else {
+    }
+    else
+    {
       analogWrite(sa2, brightness);
       analogWrite(sa1, LOW);
     }
   }
-
 }
 
 // LCD output
-int lcd_out() {
+int lcd_out()
+{
 
-  if (!isnan(humid)) {
+  if (!isnan(humid))
+  {
     lcd.setCursor(0, 0);
     lcd.print("Humidity : ");
     lcd.print(humid);
     lcd.print(" %");
   }
-  if (!isnan(temp)) {
+  if (!isnan(temp))
+  {
     lcd.setCursor(0, 1);
     lcd.print("Temp     : ");
     lcd.print(temp);
     lcd.print(" C");
   }
-  if (!isnan(value)) {
+  if (!isnan(value))
+  {
     lcd.setCursor(0, 2);
     lcd.print("Soil     : ");
     lcd.print(soil_avg);
     lcd.print(" %");
-    if (soil_avg  < treshold) {
+    if (soil_avg < treshold)
+    {
       lcd.setCursor(0, 3);
       lcd.print("* Needs watering!! *");
-    } else {
+    }
+    else
+    {
       lcd.setCursor(0, 3);
       lcd.print("                    ");
     }
@@ -372,21 +401,25 @@ int lcd_out() {
 }
 
 // Standby
-int stdby() {
+int stdby()
+{
 
   currentState = digitalRead(TouchSensor);
 
-  if (currentState == HIGH && lastState == LOW) {
+  if (currentState == HIGH && lastState == LOW)
+  {
     delay(5);
-    if (LedState == HIGH) {
+    if (lightState == HIGH)
+    {
 
-      LedState = LOW;
+      lightState = LOW;
       power = true;
       lcd.backlight();
+    }
+    else
+    {
 
-    } else {
-
-      LedState = HIGH;
+      lightState = HIGH;
       power = false;
 
       // turn off leds and screen
@@ -394,31 +427,32 @@ int stdby() {
       digitalWrite(sa2, LOW);
       digitalWrite(sa3, LOW);
       lcd.noBacklight();
-
     }
   }
   lastState = currentState;
 }
 
 // Smoothing function (soil)
-int smoothing() {
+int smoothing()
+{
 
-  total = total - readings[readIndex];   // subtract the last reading
-  readings[readIndex] = value;  // read from the soil sensor
-  total = total + readings[readIndex];  // add the reading to the total
-  readIndex = readIndex + 1; // advance to the next position in the array
+  total = total - readings[readIndex]; // subtract the last reading
+  readings[readIndex] = value;         // read from the soil sensor
+  total = total + readings[readIndex]; // add the reading to the total
+  readIndex = readIndex + 1;           // advance to the next position in the array
 
-  if (readIndex >= numReadings) {
+  if (readIndex >= numReadings)
+  {
     readIndex = 0; // re-initialize array
   }
   soil_avg = total / numReadings;
-
 }
 
-int soil_phase_print() {
+int soil_phase_print()
+{
 
   Serial.print("[ I T E R A T I O N : ");
-  Serial.print(reading_passes );
+  Serial.print(reading_passes);
   Serial.println(" ]");
   Serial.print("Current array index = ");
   Serial.println(readIndex);
@@ -430,17 +464,19 @@ int soil_phase_print() {
   Serial.print("Current total = ");
   Serial.println(total);
   Serial.println("");
-
 };
 
 // Initial test function
 int ledPins[] = {sa1, sa2, sa3};
 int currentLed;
 
-int test_init() {
-  for (int i = 0; i < 3; i++) {
+int test_init()
+{
+  for (int i = 0; i < 3; i++)
+  {
     currentLed = ledPins[i];
-    for (int j = 0; j < 10; j++) {
+    for (int j = 0; j < 10; j++)
+    {
       analogWrite(currentLed, 0);
       delay(50);
       analogWrite(currentLed, 255);
@@ -453,25 +489,28 @@ int test_init() {
 }
 
 // error handling for soil timings
-int soil_error() {
+int soil_error()
+{
 
-  if ((numReadings * inBetweenies) + 1000 > interval_soil) {
+  if ((numReadings * inBetweenies) + 1000 > interval_soil)
+  {
     Serial.println("");
     Serial.print("ERROR: Your timing on 'interval' is too low to");
     Serial.println("do the amount of 'numReadings' you want");
     Serial.println("");
   }
-
 }
 
 // wifi reset
-int wifiReset() {
+int wifiReset()
+{
   delay(1000);
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   delay(100);
   WiFi.begin();
-  if (WiFi.waitForConnectResult() == WL_CONNECTED) {
+  if (WiFi.waitForConnectResult() == WL_CONNECTED)
+  {
     WiFi.disconnect();
     while (WiFi.status() == WL_CONNECTED)
       delay(100);
@@ -480,20 +519,24 @@ int wifiReset() {
 }
 
 // stdby wifi
-int lights() {
+int lights()
+{
   currentState = HIGH;
 
-  if (currentState == HIGH && lastState == LOW) {
+  if (currentState == HIGH && lastState == LOW)
+  {
     delay(5);
-    if (LedState == HIGH) {
+    if (lightState == HIGH)
+    {
 
-      LedState = LOW;
+      lightState = LOW;
       power = true;
       lcd.backlight();
+    }
+    else
+    {
 
-    } else {
-
-      LedState = HIGH;
+      lightState = HIGH;
       power = false;
 
       // turn off leds and screen
@@ -501,10 +544,35 @@ int lights() {
       digitalWrite(sa2, LOW);
       digitalWrite(sa3, LOW);
       lcd.noBacklight();
-
     }
   }
   lastState = currentState;
 }
 
-// HTML
+int ajax()
+{
+  int a = humid;
+  int b = temp;
+  int c = soil_avg;
+  String d = "";
+
+  if (power == true)
+  {
+    d = "on";
+  }
+  else
+  {
+    d = "off";
+  }
+
+  String val = "";
+  val += a;
+  val += " ";
+  val += b;
+  val += " ";
+  val += c;
+  val += " ";
+  val += d;
+  val += " ";
+  server.send(200, "text/plain", val);
+}
